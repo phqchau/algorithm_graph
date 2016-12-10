6a8b8d2471c15ae3f9295eac9640a4024e31ce4b
Shana Weissman <shana@Shanas-Laptop.local>
2016-12-10 17:24:51 -0500

    add nodeDict

---
+++ nodeDict.py
@@ -0,0 +1,12 @@
+def nodeDict(filename):
+    nodes=open(filename, "r")
+    d = {}
+    for line in nodes.readlines():
+        lineList = line.replace("-", " ").strip().split(" ")
+        
+        for i in range(1, len(lineList)+1,2):
+            try:
+                d[lineList[0]].append(lineList[i])
+            except KeyError:
+                d[lineList[0]] = [lineList[i]]
+    print(d)
